64d9809864de7871c51bd0259d7346303d9aa431
askar <mohamedaskar476@gmail.com>
2023-01-18 07:55:40 +0530

    Folder Structure Data was created as JS object

---
+++ data/folderData.js
@@ -0,0 +1,68 @@
+// name         - name of folder/file
+// isFolder     - true => folder, false => file
+// items        - folders/files inside the folder
+
+export const folderData = [
+    {
+        id : 1,
+        name : "folder-structure-ui",
+        isFolder : true,
+        items : [
+            {
+                id : 3,
+                name : "public",
+                isFolder : true,
+                items : [
+                    {
+                        id : 7,
+                        name : "index.html",
+                        isFolder : false,
+                    }
+                ]
+            },
+            {
+                id : 4,
+                name : "src",
+                isFolder : true,
+                items : [
+                    {
+                        id : 8,
+                        name : "App.js",
+                        isFolder : false,
+                    },
+                    {
+                        id : 9,
+                        name : "Index.js",
+                        isFolder : false,
+                    },
+                    {
+                        id : 10,
+                        name : "App.css",
+                        isFolder : false,
+                    },
+                ]
+            },
+            {
+                id : 11,
+                name : "Empty Folder",
+                isFolder : true,
+                items : []
+            },
+            {
+                id : 5,
+                name : "package-lock.json",
+                isFolder : false,
+            },
+            {
+                id : 6,
+                name : "package.json",
+                isFolder : false,
+            },
+        ]
+    },
+    {
+        id : 2,
+        name : "package.json",
+        isFolder : false,
+    }
+]
