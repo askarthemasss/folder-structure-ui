1deb9ae45466f62991d57c0a9dc68d41602e7de2
askar <mohamedaskar476@gmail.com>
2023-01-19 16:53:35 +0530

    Modified src/dat/ format, completed functionality

--- src/components/Folder.js
+++ src/components/Folder.js
@@ -6,73 +6,86 @@ const Folder = ({data}) => {
 
     // console.log(data);
 
-    return(
-        <>
-            {/* <div>Folder Component</div> */}
-            {/* {data.length !== 0 && data.map(item => {
-                // If folder, add Folder Icon
-                if(item.isFolder){
-                    return (
-                        <>
-                            <div
-                                className='folder-span'
-                                key={item.id}
-                                onClick={() => setIsFolderOpen(!isFolderOpen)}
-                                // style={{zIndex:"-1"}}
-                            >
-                                <FcOpenedFolder/>
-                                {" "}{item.name}
-                                <br/>
-                            </div>
-                            {isFolderOpen && <Folder data={item.items}/>}
-                        </>
-                    )    
-                }
-                // If file, add file Icon
-                return (
-                    <div
-                        className='file-span'
-                        key={item.id}
-                    >
-                        <FcFile />
-                        {" "}{item.name}
-                        <br/>
-                    </div>
-                )
-            })} */}
-            {data.length !== 0 && data.map(item => {
-                // If folder, add Folder Icon
-                if(item.isFolder){
-                    return (
-                        <>
-                            <div
-                                className='folder-span'
-                                key={item.id}
-                                onClick={() => setIsFolderOpen(!isFolderOpen)}
-                                // style={{zIndex:"-1"}}
-                            >
-                                <FcOpenedFolder/>
-                                {" "}{item.name}
-                                <br/>
-                            </div>
-                            {isFolderOpen && <Folder data={item.items}/>}
-                        </>
-                    )    
-                }
-                // If file, add file Icon
-                return (
-                    <div
-                        className='file-span'
-                        key={item.id}
-                    >
-                        <FcFile />
-                        {" "}{item.name}
-                        <br/>
-                    </div>
-                )
-            })}
-        </>
-    )
+    // ============== Old Solution ===============
+    // return(
+    //     <>
+    //         {/* <div>Folder Component</div> */}
+    //         {data.isFolder && data.items && data.items.map(item => {
+    //             // If folder, add Folder Icon
+    //             if(item.isFolder){
+    //                 return (
+    //                     <>
+    //                         <div
+    //                             className='folder-span'
+    //                             key={item.id}
+    //                             onClick={() => setIsFolderOpen(!isFolderOpen)}
+    //                             // style={{zIndex:"-1"}}
+    //                         >
+    //                             <FcOpenedFolder/>
+    //                             {" "}{item.name}
+    //                             <br/>
+    //                         </div>
+    //                         {/* {isFolderOpen && <Folder data={item.items}/>} */}
+    //                         <div>
+    //                             {item.items.map(exp => {
+    //                                 return <Folder key={exp.id} data={exp}/>
+    //                             })}
+    //                         </div>
+    //                     </>
+    //                 )    
+    //             }
+    //             // If file, add file Icon
+    //             return (
+    //                 <div
+    //                     className='file-span'
+    //                     key={item.id}
+    //                 >
+    //                     <FcFile />
+    //                     {" "}{item.name}
+    //                     <br/>
+    //                 </div>
+    //             )
+    //         })}
+    //     </>
+    // )
+    
+    // ============ New ===================
+    if(data.isFolder){
+        // If folder, add Folder Icon
+        return (
+            <>
+                <div
+                    className='folder-span'
+                    onClick={() => setIsFolderOpen(!isFolderOpen)}
+                >
+                    <FcOpenedFolder/>
+                    {" "}{data.name}
+                </div>
+                <br/>
+                <div style={{ display: isFolderOpen ? "block" : "none", paddingLeft: 25 }}>
+                    {data.items.map(exp => {
+                        return (
+                            <Folder
+                                key={exp.id}
+                                data={exp}
+                            />
+                        )
+                    })}
+                </div>
+            </>
+        )    
+    } else {
+        // If file, add file Icon
+        return (
+            <div className='file-span' key={data.id}>
+                <FcFile />
+                {" "}{data.name}
+                <br/>
+            </div>
+        )
+    }
+
+    
 }
 
 export default Folder;
--- src/data/folderData.js
+++ src/data/folderData.js
@@ -2,67 +2,72 @@
 // isFolder     - true => folder, false => file
 // items        - folders/files inside the folder
 
-export const folderData = [
-    {
-        id : 1,
-        name : "folder-structure-ui",
-        isFolder : true,
-        items : [
-            {
-                id : 3,
-                name : "public",
-                isFolder : true,
-                items : [
-                    {
-                        id : 7,
-                        name : "index.html",
-                        isFolder : false,
-                    }
-                ]
-            },
-            {
-                id : 4,
-                name : "src",
-                isFolder : true,
-                items : [
-                    {
-                        id : 8,
-                        name : "App.js",
-                        isFolder : false,
-                    },
-                    {
-                        id : 9,
-                        name : "Index.js",
-                        isFolder : false,
-                    },
-                    {
-                        id : 10,
-                        name : "App.css",
-                        isFolder : false,
-                    },
-                ]
-            },
-            {
-                id : 11,
-                name : "Empty Folder",
-                isFolder : true,
-                items : []
-            },
-            {
-                id : 5,
-                name : "package-lock.json",
-                isFolder : false,
-            },
-            {
-                id : 6,
-                name : "package.json",
-                isFolder : false,
-            },
-        ]
-    },
-    {
-        id : 2,
-        name : "README.md",
-        isFolder : false,
-    }
-]
+export const folderData = {
+    id : 98765434567,
+    name : "root",
+    isFolder : true,
+    items : [
+        {
+            id : 1,
+            name : "folder-structure-ui",
+            isFolder : true,
+            items : [
+                {
+                    id : 3,
+                    name : "public",
+                    isFolder : true,
+                    items : [
+                        {
+                            id : 7,
+                            name : "index.html",
+                            isFolder : false,
+                        }
+                    ]
+                },
+                {
+                    id : 4,
+                    name : "src",
+                    isFolder : true,
+                    items : [
+                        {
+                            id : 8,
+                            name : "App.js",
+                            isFolder : false,
+                        },
+                        {
+                            id : 9,
+                            name : "Index.js",
+                            isFolder : false,
+                        },
+                        {
+                            id : 10,
+                            name : "App.css",
+                            isFolder : false,
+                        },
+                    ]
+                },
+                {
+                    id : 11,
+                    name : "Empty Folder",
+                    isFolder : true,
+                    items : []
+                },
+                {
+                    id : 5,
+                    name : "package-lock.json",
+                    isFolder : false,
+                },
+                {
+                    id : 6,
+                    name : "package.json",
+                    isFolder : false,
+                },
+            ]
+        },
+        {
+            id : 2,
+            name : "README.md",
+            isFolder : false,
+        }
+    ]
+}
